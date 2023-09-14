5ca3ffdb8ce7a777ab1e57c244d06a72f4298ca4
devofficer <jorisbaly1@gmail.com>
2023-09-14 08:45:24 -0700

    update

---
+++ src/config/apiEndPoints.ts
@@ -0,0 +1,1 @@
+export const EXTERNALDATA_URL = 'https://s3.amazonaws.com/deepwhale-ai-cloudformation-template-public/deepwhale-ai-front-end-exercise/deepwhaleai_sample_data_fe_1st-step.csv';
---
+++ src/config/constants.ts
@@ -0,0 +1,84 @@
+export const REGIONS = [
+	'af-south-1',
+	'ap-northeast-1',
+	'ap-northeast-2',
+	'ap-northeast-3',
+	'ap-south-1',
+	'ap-southeast-1',
+	'ap-southeast-2',
+	'ca-central-1',
+	'eu-central-1',
+	'eu-north-1',
+	'eu-west-1',
+	'eu-west-2',
+	'eu-west-3',
+	'me-south-1',
+	'sa-east-1',
+	'us-east-1',
+	'us-east-2',
+	'us-west-1',
+	'us-west-1'
+]
+
+export const INSTANCE_FAMILIES = [
+	'Not specified',
+	'c4',
+	'c5',
+	'c5a',
+	'c5n',
+	'c6a',
+	'c6i',
+	'g4dn',
+	'm3',
+	'm4',
+	'm5',
+	'm5a',
+	'm5n',
+	'm6i',
+	'r4',
+	'r5',
+	'r5a',
+	'r5n',
+	'r6i',
+	't2',
+	't3',
+	't4g'
+]
+
+export const PRODUCT_DESCRIPTIONS = [
+	'Not specified',
+	'Bring Your Own License (BYOL)',
+	'Linux/UNIX',
+	'Linux with SQL Server Enterprise',
+	'Linux with SQL Server Standard',
+	'Linux with SQL Server Web',
+	'Red Hat Enterprise Linux',
+	'Red Hat Enterprise Linux with HA',
+	'Red Hat Enterprise Linux with HA and SQL Server Enterprise',
+	'Red Hat Enterprise Linux with HA and SQL Server Standard',
+	'Red Hat Enterprise Linux with SQL Server Enterprise',
+	'Red Hat Enterprise Linux with SQL Server Standard',
+	'Red Hat Enterprise Linux with SQL Server Web',
+	'SUSE Linux',
+	'Ubuntu Pro',
+	'Windows',
+	'Windows with SQL Server Enterprise',
+	'Windows with SQL Server Standard',
+	'Windows with SQL Server Web'
+]
+
+export const PRODUCT_TYPES = [
+	'EC2',
+	'Fargate',
+	'Lamda'
+]
+
+export const TENANCIES = [
+	'shared',
+	'dedicated'
+]
+
+export const PLANS = [
+	'1 year No Upfront Compute Savings Plan',
+	'3 year No Upfront Compute Savings Plan'
+]
